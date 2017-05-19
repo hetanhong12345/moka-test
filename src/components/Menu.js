1893414de3265f9d366b206ecbd4f46616ca3f54
何坦宏 <hetanhong@mofanghr.com>
2017-05-19 10:47:05 +0800

    toggle event

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -18,6 +18,39 @@ class Menu extends Component {
 
   componentWillReceiveProps(props) {
     console.log(props);
+    let {isClear} = props;
+    if (isClear) {
+      this.setState((preState) => {
+        let state = preState;
+        state.checked = false;
+        state.menu.submenus.map(sub => {
+          sub.checked = false;
+        });
+        return state;
+      });
+    }
+  }
+
+  toggle(index) {
+    this.setState((preState) => {
+      let state = preState;
+      console.log(state.menu.submenus[index]);
+      state.menu.submenus[index]['checked'] = !state.menu.submenus[index]['checked'];
+      return state;
+
+    });
+  }
+
+  toggleAll() {
+    this.setState((preState) => {
+      let state = preState;
+      state.menu.submenus.map(sub => {
+        sub.checked = !sub.checked;
+      });
+
+      return state;
+
+    });
   }
 
   render() {
@@ -25,14 +58,20 @@ class Menu extends Component {
     return (
             <div className="menu">
               <div className="menu-item" data-flex="main:justify">
-                <span data-flex-box="0" className={classNames('box', {'checked': this.state.checked})}></span>
+                <span data-flex-box="0" onClick={() => {
+                  this.toggleAll()
+                }}
+                      className={classNames('box', {'checked': this.state.checked})}></span>
                 <span data-flex-box="1">{this.state.menu.title}</span>
                 <span data-flex-box="0">{this.state.menu.total}</span>
               </div>
               {
                 submenus.map((sub, index) => {
                   return (<div key={index} className={classNames('menu-item', 'sub')} data-flex="main:justify">
-                    <span data-flex-box="0" className={classNames('box', {'checked': sub.checked})}></span>
+                    <span data-flex-box="0" onClick={() => {
+                      this.toggle(index)
+                    }}
+                          className={classNames('box', {'checked': sub.checked})}></span>
                     <span className="sub-name" data-flex-box="1">{sub.name}</span>
                     <span data-flex-box="0">{sub.number}</span>
                   </div>)
